fde0836187efd9da627be05f4ae28f1b0a11e993
Ouarissem <ouarissemchenna79@gmail.com>
2026-07-16 10:30:41 -0700

    Add WoTMConfig for voice acting toggle

--- Core/Configuration/WoTMConfig.cs
+++ Core/Configuration/WoTMConfig.cs
@@ -3,10 +3,14 @@ using Terraria.ModLoader.Config;
 
 namespace WoTM.Core.Configuration;
 
+[BackgroundColor(20, 25, 35, 230)]
 public class WoTMConfig : ModConfig
 {
     public override ConfigScope Mode => ConfigScope.ClientSide;
 
+    [Header("$Mods.WoTM.Config.AudioSettings")]
+
+    [BackgroundColor(30, 38, 55, 245)]
     [DefaultValue(true)]
     [Label("Enable Draedon Voice Acting")]
     [Tooltip("If enabled, Draedon will speak with voice acting during dialogues. If disabled, only text will appear.")]
